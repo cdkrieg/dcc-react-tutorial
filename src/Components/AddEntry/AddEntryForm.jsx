24b8b3f3ab0dca4d4f9348ce5e2549b93f00b34b
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-03-27 08:18:24 -0500

    Added CSS to the display the data in the desired format

--- src/Components/AddEntry/AddEntryForm.jsx
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import './AddEntryForm.css';
 
 const AddEntryForm = (props) => {
+  const [weight, setWeight] = useState(0);
+  const [date, setDate] = useState("");
 
-    const [weight, setWeight] = useState(0);
-    const [date, setDate] = useState('');
-
-    function handleSubmit(event){
-        event.preventDefault();
-        let newEntry = {
-            weight: weight, date: date
-        };
-        console.log(newEntry);
-        props.addNewEntry(newEntry);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newEntry = {
+      weight: weight,
+      date: date,
+    };
+    console.log(newEntry);
+    props.addNewEntry(newEntry);
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Weight</label>
-      <input type='number' value={weight} onChange={(event) => setWeight(parseFloat(event.target.value)) }/>
-      <label>Date</label>
-      <input type='date' value={date} onChange={(event) => setDate(event.target.value) } />
-      <button type='submit'>Add</button>
+    <form onSubmit={handleSubmit} className='form-grid'>
+      <div className="form-group">
+        <label>Weight</label>
+        <input
+          type="number"
+          className="form-control"
+          value={weight}
+          onChange={(event) => setWeight(parseFloat(event.target.value))}
+        />
+      </div>
+      <div className="form-group">
+        <label>Date</label>
+        <input
+          type="date"
+          className="form-control"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-primary"
+        style={{ "marginTop": "1em" }}
+      >
+        Add
+      </button>
     </form>
   );
 };
--- src/Components/EntriesChartTracker/EntriesChaartTracker.jsx
+++ src/Components/EntriesChartTracker/EntriesChaartTracker.jsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-google-charts';
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
 
 const EntriesChartTracker = (props) => {
+  const [chartData, setChartData] = useState([]);
 
-    const [chartData, setChartData] = useState([]);
-    
-    useEffect(()=> {
-        let tempChartdata = props.parentEntries.map(entry => {
-            return [entry.date, entry.weight];
-        });
-        setChartData(tempChartdata);
-    }, [props.parentEntries]);
+  useEffect(() => {
+    let tempChartdata = props.parentEntries.map((entry) => {
+      return [entry.date, entry.weight];
+    });
+    setChartData(tempChartdata);
+  }, [props.parentEntries]);
 
-    return ( 
-        <Chart 
-            chartType='LineChart'
-            data={[['Date','Weight'], ...chartData]}
-            width='100%'
-            height='400px'
-            legendToggle
-        />
-     );
-}
- 
+  return (
+    <Chart
+      chartType="LineChart"
+      data={[["Date", "Weight"], ...chartData]}
+      width="100%"
+      height="400px"
+      options={{ legend: { position: "bottom" } }}
+      legendToggle
+    />
+  );
+};
+
 export default EntriesChartTracker;
