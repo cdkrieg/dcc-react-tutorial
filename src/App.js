efc5792fbfd5c7e9df74438e9eb1d4ab5409e1be
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-03-27 02:40:26 -0500

    Created DispalyEntries component and instantiated on App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 
 
 function App() {
+
+const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])
+
+
   return (
     <div >
-  <h3>Hello World</h3>
+      <DisplayEntries parentEntries={entries}/>
+
     </div>
   );
 }
